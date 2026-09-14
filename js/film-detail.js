631cc5a9121d2ee120be0faa5418a87b1cbc3f84
CongelaTor <congelator.contact@gmail.com>
2026-09-14 16:18:39 +0200

    responsive telephone

--- js/film-detail.js
+++ js/film-detail.js
@@ -216,8 +216,7 @@ function createFilmFileCard(film) {
   const downloadButton = document.createElement("button");
   downloadButton.className =
     "tmdb-associate-button film-file-action-button download-only";
-  // downloadButton.innerHTML = '<span class="play-icon">↓</span>';
-  downloadButton.innerHTML = '<span class="request-icon">➤</span>';
+  downloadButton.innerHTML = '<span class="play-icon">↓</span>';
   downloadButton.title = "Télécharger";
   downloadButton.onclick = () => {
     requestFilmDownload(film);
